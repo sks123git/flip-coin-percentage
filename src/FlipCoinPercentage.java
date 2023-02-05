1275003d0ab914ae4e8312a0f54cc917a603bd04
subzero <shubham8111994@gmail.com>
2023-02-05 13:17:56 +0530

    Second commit for FlipCoin program & added comments

--- src/FlipCoinPercentage.java
+++ src/FlipCoinPercentage.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class FlipCoinPercentage {
     //Program to flip a coin
-    public static double coinFlipper(){
+    public static double coinFlipper(){  //to flip and return value
         double val = (Math.random() * 1);
         return val;
     }
-
+    //Method to calculate average percentage
     public static void calculteAverage(float heads, float tails, float totalFlips){
         float avgHeads = (float) ((heads/totalFlips) * 100);
         float avgTails = (float) ((tails/totalFlips) * 100);
@@ -19,9 +19,10 @@ public class FlipCoinPercentage {
         int tailsCount = 0;
         System.out.println("Kindly enter the number of times u want to flip coin: ");
         numberOfTimes = scanner.nextInt();
-        for(int i=0; i<numberOfTimes; i++) {
+
+        for(int i=0; i<numberOfTimes; i++) {        //Flip coin no of times user want
             double flipResult = coinFlipper();
-            if (flipResult > 0.5) {
+            if (flipResult > 0.5) {                 //Condition checking and deciding whethervheads or tails
                 System.out.println("its heads " + true);
                 headsCount++;
             } else {
